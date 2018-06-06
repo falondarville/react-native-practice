823a8ee030042aa4201df04d580f68626c35b0d6
Falon Darville <falondarville@gmail.com>
2018-06-06 15:39:55 -0700

    add react-navigation package

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello World!</Text>
       </View>
     );
   }
@@ -15,6 +13,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
