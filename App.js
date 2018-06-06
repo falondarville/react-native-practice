ddc5deb6e20d2127b453753617048164e7f1a33b
Falon Darville <falondarville@gmail.com>
2018-06-06 16:13:08 -0700

    add stack navigator

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+// if you want to push unique information to each new screen, use push instead of navigate
+class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <Text>Welcome to my app!</Text>
+        <Button title="Go to details" onPress={() => this.props.navigation.navigate('Details')}/>
       </View>
     );
   }
 }
 
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is the detail page!</Text>
+        <Button title="Go home" onPress={() => this.props.navigation.navigate('Home')}/>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
