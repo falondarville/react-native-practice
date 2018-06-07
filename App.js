75cdeca7f0cb9124f6d54acc0780cc2e631f8026
Falon Darville <falondarville@gmail.com>
2018-06-06 17:15:27 -0700

    modify RootStack and header styling

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, navigationOptions } from 'react-navigation';
 
 // if you want to push unique information to each new screen, use push instead of navigate
+// the header provided with include a back button by default if there is something to go back to. But you can also programmatically add this functionality by adding this.props.navigation.goBack()
+// to go back to the home page from many pages deep, navigate to home or use navigation.popToTop()
+// you can pass information to routes using parameters in two ways. 
+// Pass the params to a route as the second parameter to the navigation function in the following way:   this.props.navigation.navigate('RouteName', { /* params go here */ })
+// navigate function or grab the param from the screen component like so: this.props.navigation.getParam(paramName, defaultValue)
+// yes you can directly access the params object with this.props.navigation.state.params, but use one of the other two methods to account for the possibility of null
+// to use params in the title, make navigationOptions into a function. Props won't work here because this is not available
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Falon's App"
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome to my app!</Text>
-        <Button title="Go to details" onPress={() => this.props.navigation.navigate('Details')}/>
+        <Button title="Go to details" onPress={() => this.props.navigation.navigate('Details')} />
       </View>
-    );
-  }
+  );
+}
 }
 
 class DetailsScreen extends React.Component {
@@ -20,6 +30,7 @@ class DetailsScreen extends React.Component {
       <View style={styles.container}>
         <Text>This is the detail page!</Text>
         <Button title="Go home" onPress={() => this.props.navigation.navigate('Home')}/>
+        <Button title="Go Back" onPress={() => this.props.navigation.goBack()}/>
       </View>
     );
   }
@@ -31,11 +42,25 @@ const RootStack = createStackNavigator(
     Details: DetailsScreen
   },
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#7fffa9"
+      },
+      headerTintColor: "#43464b",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      },
+      headerRight: (
+        <Button 
+        onPress={() => alert('This is a button')}
+        title="info"
+        color="#fff"
+        />
+      ),
+    },
   }
 );
-
-
 
 const styles = StyleSheet.create({
   container: {
